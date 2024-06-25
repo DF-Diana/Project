ac56ffccb1a2394e64ab49b1dfffa56a2685641e
DF-Diana <dianag.funes@gmail.com>
2024-06-25 14:38:27 -0600

    cine.py

--- cine.py
+++ cine.py
@@ -60,7 +60,7 @@ class cine:
                     compras.append((name, numBoletos, numPersonas, tarjeta, total))
         return compras
 
-#comentario
+#comentario de notificacion
 
 '''def guardarInformacion(self, numBoletos, total, name, tarjeta, numPersonas):
     self.numBoletos = numBoletos
